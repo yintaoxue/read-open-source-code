14dbc648535a5951b9c9d87a05e0fc0fb7565aaa
Talin <yintaoxue@gmail.com>
2014-02-01 16:58:30 +0800

    test ArrayUtils.toMap

--- lang3/src/main/java/org/ruogu/lang3/ArrayUtilsTest.java
+++ lang3/src/main/java/org/ruogu/lang3/ArrayUtilsTest.java
@@ -16,6 +16,10 @@
  */
 package org.ruogu.lang3;
 
+import java.util.AbstractMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
 import org.apache.commons.lang3.ArrayUtils;
 
 public class ArrayUtilsTest {
@@ -23,8 +27,26 @@ public class ArrayUtilsTest {
 		Class<?>[] classArray = new Class[0];
 
 		int[][] intArray = { { 1, 4, 6 }, { 2, 0, 7 } };
-		
+
 		System.out.println(ArrayUtils.toString(intArray));
+
+		// 字符串数组转map
+		String[][] toMap1 = { { "k1", "v1" }, { "k2", "v2", "vv" } };
+		Map<Object, Object> map1 = ArrayUtils.toMap(toMap1);
+		System.out.println(map1.toString());
+		
+		// Entry数组转amp
+		Entry<String, String> o1 = new AbstractMap.SimpleEntry<String, String>("key1", "value1");
+		Entry<String, String> o2 = new AbstractMap.SimpleEntry<String, String>("key2", "value2");
+		Entry[] jj = new AbstractMap.SimpleEntry[2];
+		jj[0] = o1;
+		jj[1] = o2;
+		
+		System.out.println(ArrayUtils.toMap(jj));
+		
+		// 如何创建数组
+		Entry<String, String>[] jjs = new AbstractMap.SimpleEntry[2];
+		Entry<String, String>[] jjss = new Entry[2];
 		
 	}
 }
